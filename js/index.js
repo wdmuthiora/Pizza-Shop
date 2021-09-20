edc66a6989ac62b1e78a64bc5e665b3f143fb5ec
wdmuthiora <wdmuthiora@gmail.com>
2021-09-21 00:02:58 +0300

    Added Menu section.

---
+++ js/index.js
@@ -0,0 +1,72 @@
+//pizza1
+$(document).ready(function () {
+  $("#pizza1").hover(function () {
+    $(this).css("opacity", "0.35");
+  }, function () {
+    $(this).css("opacity", "1.0");
+  })
+})
+
+//pizza2
+$(document).ready(function () {
+  $("#pizza2").hover(function () {
+    $(this).css("opacity", "0.35");
+  }, function () {
+    $(this).css("opacity", "1.0");
+  })
+})
+
+//pizza3
+$(document).ready(function () {
+  $("#pizza3").hover(function () {
+    $(this).css("opacity", "0.35");
+  }, function () {
+    $(this).css("opacity", "1.0");
+  })
+})
+
+//pizza4
+$(document).ready(function () {
+  $("#pizza4").hover(function () {
+    $(this).css("opacity", "0.35");
+  }, function () {
+    $(this).css("opacity", "1.0");
+  })
+})
+
+//pizza5
+$(document).ready(function () {
+  $("#pizza5").hover(function () {
+    $(this).css("opacity", "0.35");
+  }, function () {
+    $(this).css("opacity", "1.0");
+  })
+})
+
+//pizza6
+$(document).ready(function () {
+  $("#pizza6").hover(function () {
+    $(this).css("opacity", "0.35");
+  }, function () {
+    $(this).css("opacity", "1.0");
+  })
+})
+
+//pizza7
+$(document).ready(function () {
+  $("#pizza7").hover(function () {
+    $(this).css("opacity", "0.35");
+  }, function () {
+    $(this).css("opacity", "1.0");
+  })
+})
+
+
+//pizza8
+$(document).ready(function () {
+  $("#pizza8").hover(function () {
+    $(this).css("opacity", "0.35");
+  }, function () {
+    $(this).css("opacity", "1.0");
+  })
+})
